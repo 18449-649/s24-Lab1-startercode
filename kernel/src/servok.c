9c07efde691ce1f374fcd932c7f82bbf9fe02041
Sruti Srinidhi <ssrinidh@andrew.cmu.edu>
2024-02-05 14:28:19 -0800

    provided servo init code

--- kernel/src/servok.c
+++ kernel/src/servok.c
@@ -13,9 +13,20 @@
 #include <servok.h>
 #include <timer.h>
 
+#define SERVO0_PORT GPIO_B
+#define SERVO0_PINNUM 10
+
+#define SERVO1_PORT GPIO_A
+#define SERVO1_PINNUM 8
+
+#define TIMER_FREQ 100000
+
 #define UNUSED __attribute__((unused))
 
 void servo_init() {
+  gpio_init( SERVO0_PORT, SERVO0_PINNUM, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_VERY_HIGH, PUPD_NONE, ALT0 );
+  gpio_init( SERVO1_PORT, SERVO1_PINNUM, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_VERY_HIGH, PUPD_NONE, ALT0 );
+  timer_start( TIMER_FREQ );
 }
 
 int sys_servo_enable(UNUSED uint8_t channel, UNUSED uint8_t enabled){
